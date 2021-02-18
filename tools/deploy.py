f604513cdd8c0dda7d6b441998ca5049e2357651
LengSword <1030588973@qq.com>
2021-02-18 23:32:40 +0800

    XL-v1.0.0

--- tools/deploy.py
+++ tools/deploy.py
@@ -5,14 +5,14 @@ version = ''
 
 with open('CHANGELOG.md', encoding='utf-8') as f:
     first_line = f.readline()
-    version = re.match(r'## v(\d\.\d+)', first_line).group(1)
+    version = re.match(r'## v(\d+\.\d+.\d+)', first_line).group(1)
 
 if not version:
     exit()
 
 os.chdir('./releases')
 
-map_fullname = f'X_Hero_Siege_{version}.w3x'
+map_fullname = f'XL_Hero_Siege_{version}.w3x'
 original_mapname = 'SlkMap.w3x'
 
 if os.path.exists(map_fullname):
